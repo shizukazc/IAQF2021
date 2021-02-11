0647e8e928d66e0063a2517abb190816a20753b4
Bohan Zhao <bz2086@nyu.edu>
2021-02-11 03:06:38 -0500

    we can use data_importer in code1 code 2 now

---
+++ Code/Code2/iaqf_data_importer.py
@@ -0,0 +1,28 @@
+#SECRET = 666
+#P1
+#entire
+#period1
+#period2
+#P2
+#entire
+#period1
+#period2
+#P3
+#entire
+#period1
+#period2
+import numpy as np
+import pandas as pd
+#import os
+df_automobile_sale_01_08 = pd.read_csv('../../Data/cleansed_data/automobile_sale_01_08.csv')
+df_automobile_sale_09_19 = pd.read_csv('../../Data/cleansed_data/automobile_sale_09_19.csv')
+df_coal_consump_01_08 = pd.read_csv('../../Data/cleansed_data/coal_consump_01_08.csv')
+df_coal_consump_09_19 = pd.read_csv('../../Data/cleansed_data/coal_consump_09_19.csv')
+df_CPI_01_08 = pd.read_csv('../../Data/cleansed_data/CPI_01_08.csv')
+df_CPI_09_19 = pd.read_csv('../../Data/cleansed_data/CPI_09_19.csv')
+df_natural_gas_consump_01_08 = pd.read_csv('../../Data/cleansed_data/natural_gas_consump_01_08.csv')
+df_natural_gas_consump_09_19 = pd.read_csv('../../Data/cleansed_data/natural_gas_consump_09_19.csv')
+df_treescover_loss_01_08 = pd.read_csv('../../Data/cleansed_data/treescover_loss_01_08.csv')
+df_treescover_loss_09_19 = pd.read_csv('../../Data/cleansed_data/treescover_loss_09_19.csv')
+df_US_real_GDP_01_08 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_01_08.csv')
+df_US_real_GDP_09_19 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_09_19.csv')
