1c3dd6746dfb5e4d7dd6b5ac26a2ed3d2b5bb8dc
Bohan Zhao <bz2086@nyu.edu>
2021-02-11 22:15:13 -0500

    update data_importer

---
+++ Code/iaqf_data_importer.py
@@ -0,0 +1,77 @@
+#SECRET = 666
+import numpy as np
+import pandas as pd
+#import os
+
+#P1
+#entire
+# df_entire_p1 = 
+
+#period1
+# df_firper_p1 = 
+df_CPI_01_08 = pd.read_csv('../../Data/cleansed_data/CPI_01_08.csv')
+df_coal_consump_01_08 = pd.read_csv('../../Data/cleansed_data/coal_consump_01_08.csv')
+df_natural_gas_consump_01_08 = pd.read_csv('../../Data/cleansed_data/natural_gas_consump_01_08.csv')
+# need US population csv(STEP)
+# need crude oil production csv
+df_treescover_loss_01_08 = pd.read_csv('../../Data/cleansed_data/treescover_loss_01_08.csv')
+df_US_real_GDP_01_08 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_01_08.csv')
+
+#period2
+# df_secper_p1 = 
+df_CPI_09_19 = pd.read_csv('../../Data/cleansed_data/CPI_09_19.csv')
+df_coal_consump_09_19 = pd.read_csv('../../Data/cleansed_data/coal_consump_09_19.csv')
+df_natural_gas_consump_09_19 = pd.read_csv('../../Data/cleansed_data/natural_gas_consump_09_19.csv')
+# need US population csv(STEP)
+# need crudeoil production csv
+df_treescover_loss_09_19 = pd.read_csv('../../Data/cleansed_data/treescover_loss_09_19.csv')
+df_US_real_GDP_09_19 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_09_19.csv')
+
+#P2
+#entire
+# df_entire_p2 = 
+
+#period1
+# df_firper_p2 = 
+df_CPI_01_08 = pd.read_csv('../../Data/cleansed_data/CPI_01_08.csv')
+# need US populaiton csv
+# need public roads csv
+# need US urban populaiton csv
+# need crude oil production csv
+df_automobile_sale_01_08 = pd.read_csv('../../Data/cleansed_data/automobile_sale_01_08.csv')
+df_US_real_GDP_01_08 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_01_08.csv')
+
+#period2
+# df_secper_p2 = 
+df_CPI_09_19 = pd.read_csv('../../Data/cleansed_data/CPI_09_19.csv')
+# need US populaiton csv
+# need public roads csv
+# need US urban populaiton csv
+# need crude oil production csv
+df_automobile_sale_09_19 = pd.read_csv('../../Data/cleansed_data/automobile_sale_09_19.csv')
+df_US_real_GDP_09_19 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_09_19.csv')
+
+#P3
+#entire
+# df_entire_p3 = 
+
+#period1
+# df_firper_p3 = 
+df_CPI_01_08 = pd.read_csv('../../Data/cleansed_data/CPI_01_08.csv')
+# need US populaiton csv
+# need public roads csv
+# need US urban populaiton csv
+# need crude oil production csv
+df_automobile_sale_01_08 = pd.read_csv('../../Data/cleansed_data/automobile_sale_01_08.csv')
+df_US_real_GDP_01_08 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_01_08.csv')
+
+#period2
+# df_secper_p3 = 
+df_CPI_09_19 = pd.read_csv('../../Data/cleansed_data/CPI_09_19.csv')
+# need US populaiton csv
+# need public roads csv
+# need US urban populaiton csv
+# need crude oil production csv
+df_automobile_sale_09_19 = pd.read_csv('../../Data/cleansed_data/automobile_sale_09_19.csv')
+df_US_real_GDP_09_19 = pd.read_csv('../../Data/cleansed_data/US_real_GDP_09_19.csv')
+
